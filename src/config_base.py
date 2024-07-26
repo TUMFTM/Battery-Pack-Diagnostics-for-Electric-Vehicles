4e0b768cc22c984cec581efafd64e6a0711feac7
Philip Bilfinger <ga97req@mytum.de>
2024-07-26 09:28:11 +0200

    changed paths to work with repo name

--- src/config_base.py
+++ src/config_base.py
@@ -5,7 +5,9 @@ class GeneralConfig(enum.Enum):
     """
     Holds the general config values for the VCA Paper
     """
-        path2data = os.path.join(os.getcwd().partition('Code')[0], "Code","data")
+    path2data = os.path.join(os.getcwd().partition('Battery-Pack-Diagnostics-for-Electric-Vehicles')[0],
+                             "Battery-Pack-Diagnostics-for-Electric-Vehicles",
+                             "data")
     path2font = os.path.join(path2data, "font")
     
     path2figures = os.path.join(path2data,  "figures")
